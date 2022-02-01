4a22f04e50fe4c9dba126bc287a8dba5843be601
Koby Ingram <kobyingram@Kobys-MacBook-Air.local>
2022-02-01 13:24:33 -0600

    updating files

--- script.js
+++ script.js
@@ -3,15 +3,54 @@
 // .ready() = method of $jQ
 // .ready( ... ) = argument/parameter passed to that method
 // function(){} in the method is an IIFE = Immediately Invoked F/n Expresson
-$(document).ready(function () {
-  // alert("page is ready");
+$(document).ready(function(){ 
+  //alert("page is ready");
   console.log("doc is ready");
 
-  let userGreeting = "Hello there, ";
+let wowCount = 0;
+let wowMsg;
 
-  $("button").click(function () {
-    userGreeting += $("#fname").val();
 
-    $("#greetingOutput").text(userGreeting);
-  });
+$( "#dogeDiv" ).draggable(
+
+  {
+    containment: "#dogeHouse", 
+    scroll:false,
+    start: function() {   //start
+      wowCount++;
+      console.log("we're starting.", "this is wowCount", wowCount);
+
+    if (wowCount <= 5){
+      wowMsg = "slight wow. ";
+    }
+    else if(wowCount <=10){
+      wowMsg = "halfwow there. ";
+    }
+    else{
+      wowMsg = "too much wow.";
+    }
+  
+
+      $("#wowOutput").text(wowMsg +"you has " + wowCount +  " wow");
+    
+    }//start
+});
+
+
+$("#dogeDiv").draggable();
+
+
+  // let userGreeting = "You are enough, ";
+  
+  // $("button").click(function(){
+  //   userGreeting = userGreeting + $("#fname").val() + "...just as you are.";
+    
+  //   $("#greetingOutput").text(userGreeting);
+    
+    
+  //   //console.log($("#fname").val());
+
+
+    
+  // });
 });
